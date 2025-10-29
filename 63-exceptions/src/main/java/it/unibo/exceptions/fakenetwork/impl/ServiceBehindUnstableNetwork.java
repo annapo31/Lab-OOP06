272b485b51427b51244877b523947e7cca3373c1
Anna <annapollini2005@gmail.com>
2025-10-29 18:55:43 +0100

    Modified function sendData in ServiceBehindUnstableNetwork - pt.4

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -59,7 +59,7 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
             commandQueue.add(data);
         } else {
             final var message = data + " is not a valid keyword (allowed: " + KEYWORDS + "), nor is a number";
-            System.out.println(message);
+            // If data is not valid -> empty the queue
             commandQueue.clear();
             /*
              * This method, in this point, should throw an IllegalStateException.
@@ -68,6 +68,7 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
              *
              * The previous exceptions must be set as the cause of the new exception
              */
+            throw new IllegalArgumentException(message);
         }
     }
 
